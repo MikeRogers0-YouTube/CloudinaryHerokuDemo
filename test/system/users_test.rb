8c74160c9251effb063a973723c42179253d6fd5
Mike Rogers <me@mikerogers.io>
2017-12-26 13:25:49 +0000

    Working on testing with ApplicationSystemTestCase

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,9 +1,15 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit users_url
-  #
-  #   assert_selector "h1", text: "User"
-  # end
+  test "Creating a user with an Avatar" do
+    visit users_url
+    click_on "New User"
+    
+    fill_in 
+    attach_file 'user_avatar', Rails.root.join('test/fixtures/files/1x1-ffffffff.png')
+    click_on 'Create User'
+
+
+    assert_selector "h1", text: "User"
+  end
 end
