6f97ee38f2854dc200be191a83a4c95b9a2b14ae
Mike Rogers <me@mikerogers.io>
2017-12-27 11:32:27 +0000

    Added one (noisey) API test

--- app/uploaders/application_uploader.rb
+++ app/uploaders/application_uploader.rb
@@ -9,4 +9,17 @@ class ApplicationUploader < CarrierWave::Uploader::Base
       "#{model.class.to_s.parameterize}_#{model.id}"
     ].join(',')
   end
+
+  def direct_uploading_tags
+    [tags, 'db_unsaved'].join(',')
+  end
+
+  # When we upload, update the tags (removing the db_unsaved tag)
+  def process!(new_file=nil)
+    Cloudinary::Api.update(my_public_id, tags: tags)
+  end
+
+  def cloudinary_resource
+    Cloudinary::Api.resource(my_public_id)
+  end
 end
--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -1,14 +1,25 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
+  # This test will hit the external API
   test "Creating a user with an Avatar" do
-    pending
+    user_count = User.count
+
     visit users_url
     click_on "New User"
     
     fill_in 'user_name', with: 'Mr User Name'
-    attach_file 'user_avatar', Rails.root.join('test/fixtures/files/1x1-ffffffff.png')
+    attach_file 'user_avatar', file_fixture('1x1-ffffffff.png')
     click_on 'Create User'
 
+    assert_selector('#notice', text: 'User was successfully created.')
+
+    # Verify the DB saved correctly
+    assert(User.count, user_count + 1)
+    assert(User.last.name, 'Mr User Name')
+    assert(User.last.avatar.present?, true)
+
+    # Verify cloudinary processed the image.
+    assert(User.last.avatar.cloudinary_resource['tags'].join(','), User.last.avatar.tags)
   end
 end
