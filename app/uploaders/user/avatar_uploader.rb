89980c9647605159db2c3800db92c57fb203dff7
Mike Rogers <me@mikerogers.io>
2018-01-03 16:32:43 +0000

    Adding example links above versions

--- app/uploaders/user/avatar_uploader.rb
+++ app/uploaders/user/avatar_uploader.rb
@@ -1,4 +1,5 @@
 class User::AvatarUploader < ApplicationUploader
+  # http://res.cloudinary.com/demo/image/upload/c_fill,f_auto,h_180,q_auto,w_180/sample.png
   version :default do    
     eager
     #process resize_to_limit: [150, 150]
@@ -6,12 +7,14 @@ class User::AvatarUploader < ApplicationUploader
     cloudinary_transformation quality: 'auto', fetch_format: 'auto'
   end
 
+  # http://res.cloudinary.com/demo/image/upload/c_fill,h_48,r_max,w_48/sample.ico
   version :favicon do    
     process convert: 'ico'
     process resize_to_fill: [48, 48]
     cloudinary_transformation radius: 'max'
   end
 
+  # http://res.cloudinary.com/demo/image/upload/c_fill,h_114,w_114/sample.png
   version :apple_touch do    
     process convert: 'png'
     process resize_to_fill: [114, 114]
